edf6c0532fe9422a22d7286cb94c6df8d3c1db0b
nucccio <ca591sav@htwg-konstanz.de>
2025-12-03 14:43:55 +0100

    Iteration 0 - generated on https://start.spring.io/ with Spring Web and Spring Data JPA added + gitignore for mac

---
+++ src/main/java/de/htwg/in/schneider/casellese/backend_casellese/BackendCaselleseApplication.java
@@ -0,0 +1,13 @@
+package de.htwg.in.schneider.casellese.backend_casellese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCaselleseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCaselleseApplication.class, args);
+	}
+
+}
